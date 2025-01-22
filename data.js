c2a4d7d2daa570eb06a3fa53a0e411ea1dcded89
Celal <binaycelalselim@gmail.com>
2025-01-23 02:09:55 +0300

    Data.js added
    Co-authored-by: wiseweb-works <wisewebworks@outlook.com>
    Co-authored-by: yavuzahmet1 <1yavuzahmet@gmail.com>
    Co-authored-by: zbaharyilmaz <ylmzbhr1@gmail.com>
    Co-authored-by: nur2810 <sseymanurarslan@gmail.com>
    Co-authored-by: ImrenR <imrenrahbay@gmail.com>
    Co-authored-by: Celal-Dzelal <binaycelalselim@gmail.com>

---
+++ data.js
@@ -0,0 +1,632 @@
+export const hallOfFamePlayers = [
+  {
+    id: 1,
+    name: "Jerry Rice",
+    team: "San Francisco 49ers",
+    teamsPlayed: ["San Francisco 49ers", "Oakland Raiders", "Seattle Seahawks"],
+    statistics: {
+      receptions: 1549,
+      receivingYards: 22895,
+      receivingTouchdowns: 197,
+    },
+    yearsActive: "1985-2004",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYyXtXlUd8nyunQ02QsSVMYPYbyBibby3Iv5R7VOn3N2ktFlKV15f7Q8d88xD_BrahZE32kXqSgIJQNl6xAO0giA",
+  },
+  {
+    id: 2,
+    name: "Emmitt Smith",
+    team: "Dallas Cowboys",
+    teamsPlayed: ["Dallas Cowboys", "Arizona Cardinals"],
+    statistics: {
+      rushingYards: 18355,
+      rushingTouchdowns: 164,
+      receptions: 515,
+      receivingYards: 3224,
+      receivingTouchdowns: 11,
+    },
+    yearsActive: "1990-2004",
+    photoUrl:
+      "https://static.clubs.nfl.com/image/private/t_landscape_tablet/cowboys/j7rseorb177wsx2pd9an.jpg",
+  },
+  {
+    id: 3,
+    name: "Joe Montana",
+    team: "San Francisco 49ers",
+    teamsPlayed: ["San Francisco 49ers", "Kansas City Chiefs"],
+    statistics: {
+      completions: 3409,
+      attempts: 5391,
+      passingYards: 40551,
+      passingTouchdowns: 273,
+      interceptions: 139,
+    },
+    yearsActive: "1979-1994",
+    photoUrl:
+      "https://orderisda.org/wp-content/uploads/2021/02/Screen-Shot-2021-02-06-at-12.01.10-PM.jpeg",
+  },
+  {
+    id: 4,
+    name: "Reggie White",
+    team: "Green Bay Packers",
+    teamsPlayed: [
+      "Philadelphia Eagles",
+      "Green Bay Packers",
+      "Carolina Panthers",
+    ],
+    statistics: {
+      sacks: 198,
+      forcedFumbles: 33,
+      fumbleRecoveries: 20,
+    },
+    yearsActive: "1985-2000",
+    photoUrl:
+      "https://www.knoxnews.com/gcdn/-mm-/ec81d60c53309e404249555bab1cff38782a0fdc/c=0-107-1698-1066/local/-/media/2018/05/21/WIGroup/PackersNews/636624988413983135-reggie1-12-2f20-2f98.tif.jpg",
+  },
+  {
+    id: 5,
+    name: "Walter Payton",
+    team: "Chicago Bears",
+    teamsPlayed: ["Chicago Bears"],
+    statistics: {
+      rushingYards: 16726,
+      rushingTouchdowns: 110,
+      receptions: 492,
+      receivingYards: 4538,
+      receivingTouchdowns: 15,
+    },
+    yearsActive: "1975-1987",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQekDPUTZ5A_qSdgctbq_x-HFV2aGa5_wjuWw&s",
+  },
+  {
+    id: 6,
+    name: "Lawrence Taylor",
+    team: "New York Giants",
+    teamsPlayed: ["New York Giants"],
+    statistics: {
+      sacks: 132.5,
+      interceptions: 9,
+      fumbleRecoveries: 11,
+    },
+    yearsActive: "1981-1993",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgoTWcRxlsKn6YtwaDm9SBQZvcwxUl684gYQ&s",
+  },
+  {
+    id: 7,
+    name: "Barry Sanders",
+    team: "Detroit Lions",
+    teamsPlayed: ["Detroit Lions"],
+    statistics: {
+      rushingYards: 15269,
+      rushingTouchdowns: 99,
+      receptions: 352,
+      receivingYards: 2921,
+      receivingTouchdowns: 10,
+    },
+    yearsActive: "1989-1998",
+    photoUrl:
+      "https://cdn.vox-cdn.com/thumbor/kOSINFVutMTYsE0ae9E2gXOSpc0=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/18317859/81672118.jpg.jpg",
+  },
+  {
+    id: 8,
+    name: "Dan Marino",
+    team: "Miami Dolphins",
+    teamsPlayed: ["Miami Dolphins"],
+    statistics: {
+      completions: 4967,
+      attempts: 8358,
+      passingYards: 61361,
+      passingTouchdowns: 420,
+      interceptions: 252,
+    },
+    yearsActive: "1983-1999",
+    photoUrl:
+      "https://static.www.nfl.com/image/private/t_editorial_landscape_12_desktop/league/xejjnpm0no7q439jhlvr",
+  },
+  {
+    id: 9,
+    name: "Brett Favre",
+    team: "Green Bay Packers",
+    teamsPlayed: [
+      "Atlanta Falcons",
+      "Green Bay Packers",
+      "New York Jets",
+      "Minnesota Vikings",
+    ],
+    statistics: {
+      completions: 6300,
+      attempts: 10169,
+      passingYards: 71838,
+      passingTouchdowns: 508,
+      interceptions: 336,
+    },
+    yearsActive: "1991-2010",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWsKqTnQSoQOPLor94VwJv9qU8CNaczq4LnA&s",
+  },
+  {
+    id: 10,
+    name: "Peyton Manning",
+    team: "Indianapolis Colts",
+    teamsPlayed: ["Indianapolis Colts", "Denver Broncos"],
+    statistics: {
+      completions: 6125,
+      attempts: 9380,
+      passingYards: 71940,
+      passingTouchdowns: 539,
+      interceptions: 251,
+    },
+    yearsActive: "1998-2015",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfuwuwa23eEJMTuLB01IdrHxqXzaw4GF9NkA&s",
+  },
+  {
+    id: 11,
+    name: "Tom Brady",
+    team: "New England Patriots",
+    teamsPlayed: ["New England Patriots", "Tampa Bay Buccaneers"],
+    statistics: {
+      completions: 7753,
+      attempts: 12350,
+      passingYards: 89514,
+      passingTouchdowns: 649,
+      interceptions: 212,
+    },
+    yearsActive: "2000-2022",
+    photoUrl:
+      "https://www.thenation.com/wp-content/uploads/2018/01/tom-brady-patriots-jaguars-ap_img.jpg",
+  },
+  {
+    id: 12,
+    name: "Deion Sanders",
+    team: "Dallas Cowboys",
+    teamsPlayed: [
+      "Atlanta Falcons",
+      "San Francisco 49ers",
+      "Dallas Cowboys",
+      "Washington Redskins",
+      "Baltimore Ravens",
+    ],
+    statistics: {
+      interceptions: 53,
+      touchdowns: 22,
+      puntReturnYards: 212,
+    },
+    yearsActive: "1989-2005",
+    photoUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/dallas-cowboys-cornerback-deion-sanders-outdistances-news-photo-1694021710.jpg",
+  },
+  {
+    id: 13,
+    name: "Tony Gonzalez",
+    team: "Kansas City Chiefs",
+    teamsPlayed: ["Kansas City Chiefs", "Atlanta Falcons"],
+    statistics: {
+      receptions: 1325,
+      receivingYards: 15127,
+      receivingTouchdowns: 111,
+    },
+    yearsActive: "1997-2013",
+    photoUrl:
+      "https://static.clubs.nfl.com/image/private/f_auto/chiefs/whcr4nls9ykcptzgwvpd",
+  },
+  {
+    id: 14,
+    name: "Randy Moss",
+    team: "Minnesota Vikings",
+    teamsPlayed: [
+      "Minnesota Vikings",
+      "Oakland Raiders",
+      "New England Patriots",
+      "Tennessee Titans",
+      "San Francisco 49ers",
+    ],
+    statistics: {
+      receptions: 982,
+      receivingYards: 15292,
+      receivingTouchdowns: 156,
+    },
+    yearsActive: "1998-2012",
+    photoUrl:
+      "https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/vikings/yrc3216oxt8stt9tjjnu",
+  },
+  {
+    id: 15,
+    name: "Ray Lewis",
+    team: "Baltimore Ravens",
+    teamsPlayed: ["Baltimore Ravens"],
+    statistics: {
+      tackles: 2059,
+      sacks: 41.5,
+      interceptions: 31,
+    },
+    yearsActive: "1996-2012",
+    photoUrl:
+      "https://www.usatoday.com/gcdn/media/USATODAY/USATODAY/2012/10/15/ray-lewis-injury-ravens-10-15-12-16_9.jpg?width=660&height=373&fit=crop&format=pjpg&auto=webp",
+  },
+  {
+    id: 16,
+    name: "Troy Polamalu",
+    team: "Pittsburgh Steelers",
+    teamsPlayed: ["Pittsburgh Steelers"],
+    statistics: {
+      tackles: 770,
+      interceptions: 32,
+      touchdowns: 3,
+    },
+    yearsActive: "2003-2014",
+    photoUrl:
+      "https://9b16f79ca967fd0708d1-2713572fef44aa49ec323e813b06d2d9.ssl.cf2.rackcdn.com/1140x_a10-7_cTC/troy-polamalu-103-1580595813.jpg",
+  },
+  {
+    id: 17,
+    name: "Calvin Johnson",
+    team: "Detroit Lions",
+    teamsPlayed: ["Detroit Lions"],
+    statistics: {
+      receptions: 731,
+      receivingYards: 11619,
+      receivingTouchdowns: 83,
+    },
+    yearsActive: "2007-2015",
+    photoUrl:
+      "https://media.mlive.com/lions_impact/photo/calvin-johnson-045495d3e5c74830.jpg",
+  },
+  {
+    id: 18,
+    name: "Steve Young",
+    team: "San Francisco 49ers",
+    teamsPlayed: ["Tampa Bay Buccaneers", "San Francisco 49ers"],
+    statistics: {
+      completions: 2667,
+      attempts: 4149,
+      passingYards: 33124,
+      passingTouchdowns: 232,
+      interceptions: 107,
+    },
+    yearsActive: "1985-1999",
+    photoUrl:
+      "https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/49ers/xz7xi5ntwnmis5ixd01r",
+  },
+  {
+    id: 19,
+    name: "Drew Brees",
+    team: "New Orleans Saints",
+    teamsPlayed: ["San Diego Chargers", "New Orleans Saints"],
+    statistics: {
+      completions: 7142,
+      attempts: 10551,
+      passingYards: 80058,
+      passingTouchdowns: 571,
+      interceptions: 243,
+    },
+    yearsActive: "2001-2020",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDaRkOEATxFlbIxr9Nw_BhnB4Kb6wdU11nQQ&s",
+  },
+  {
+    id: 20,
+    name: "Ed Reed",
+    team: "Baltimore Ravens",
+    teamsPlayed: ["Baltimore Ravens", "Houston Texans", "New York Jets"],
+    statistics: {
+      interceptions: 64,
+      tackles: 643,
+      touchdowns: 9,
+    },
+    yearsActive: "2002-2013",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs0GbMl0T5ZkvsuzJ6854qHzO0aC-octXcAg&s",
+  },
+  {
+    id: 21,
+    name: "Aaron Rodgers",
+    team: "Green Bay Packers",
+    teamsPlayed: ["Green Bay Packers"],
+    statistics: {
+      completions: 4759,
+      attempts: 7282,
+      passingYards: 55962,
+      passingTouchdowns: 449,
+      interceptions: 93,
+    },
+    yearsActive: "2005-Present",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHH1sTcNIbfY68MrIc8h_wtu4lKvq1-Vohsg&s",
+  },
+  {
+    id: 22,
+    name: "Jim Brown",
+    team: "Cleveland Browns",
+    teamsPlayed: ["Cleveland Browns"],
+    statistics: {
+      rushingYards: 12312,
+      rushingTouchdowns: 106,
+      receptions: 262,
+      receivingYards: 2499,
+      receivingTouchdowns: 20,
+    },
+    yearsActive: "1957-1965",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgQlhFB6AV4Xvs6ZVg4vNQGjFdC43lBfhiAA&s",
+  },
+  {
+    id: 23,
+    name: "Patrick Mahomes",
+    team: "Kansas City Chiefs",
+    teamsPlayed: ["Kansas City Chiefs"],
+    statistics: {
+      completions: 1545,
+      attempts: 2235,
+      passingYards: 18991,
+      passingTouchdowns: 151,
+      interceptions: 37,
+    },
+    yearsActive: "2017-Present",
+    photoUrl:
+      "https://media.cnn.com/api/v1/images/stellar/prod/230110173341-patrick-mahomes.jpg?c=16x9&q=h_833,w_1480,c_fill",
+  },
+  {
+    id: 24,
+    name: "Eric Dickerson",
+    team: "Los Angeles Rams",
+    teamsPlayed: [
+      "Los Angeles Rams",
+      "Indianapolis Colts",
+      "Los Angeles Raiders",
+      "Atlanta Falcons",
+    ],
+    statistics: {
+      rushingYards: 13259,
+      rushingTouchdowns: 90,
+      receptions: 281,
+      receivingYards: 2554,
+      receivingTouchdowns: 6,
+    },
+    yearsActive: "1983-1993",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKvDik2Snm6NwXvf1RzMQM3mW3yjTxeLmc-A&s",
+  },
+  {
+    id: 25,
+    name: "Roger Staubach",
+    team: "Dallas Cowboys",
+    teamsPlayed: ["Dallas Cowboys"],
+    statistics: {
+      completions: 1685,
+      attempts: 2958,
+      passingYards: 22700,
+      passingTouchdowns: 153,
+      interceptions: 109,
+    },
+    yearsActive: "1969-1979",
+    photoUrl:
+      "https://images2.minutemediacdn.com/image/upload/c_crop,w_2700,h_1518,x_0,y_644/c_fill,w_912,ar_16:9,f_auto,q_auto,g_auto/images/ImagnImages/mmsport/cowboys_country/01j69ywdev7whecg2e2j.jpg",
+  },
+  {
+    id: 26,
+    name: "Bruce Smith",
+    team: "Buffalo Bills",
+    teamsPlayed: ["Buffalo Bills", "Washington Redskins"],
+    statistics: {
+      sacks: 200,
+      tackles: 1225,
+      forcedFumbles: 43,
+    },
+    yearsActive: "1985-2003",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKbqP_VyUIS73D2xT9zWiPavkTW0qEgkrRDg&s",
+  },
+  {
+    id: 27,
+    name: "Bart Starr",
+    team: "Green Bay Packers",
+    teamsPlayed: ["Green Bay Packers"],
+    statistics: {
+      completions: 1808,
+      attempts: 3149,
+      passingYards: 24718,
+      passingTouchdowns: 152,
+      interceptions: 138,
+    },
+    yearsActive: "1956-1971",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmEUX10bTlP8G6mNjTFh2Xqmt29oKzzj4_SQ&s",
+  },
+  {
+    id: 28,
+    name: "O.J. Simpson",
+    team: "Buffalo Bills",
+    teamsPlayed: ["Buffalo Bills", "San Francisco 49ers"],
+    statistics: {
+      rushingYards: 11236,
+      rushingTouchdowns: 61,
+      receptions: 203,
+      receivingYards: 2142,
+      receivingTouchdowns: 14,
+    },
+    yearsActive: "1969-1979",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-g7cW9ghUUJSf5-oVsfi3NZBGQ7C4sz5o0A&s",
+  },
+  {
+    id: 29,
+    name: "Michael Irvin",
+    team: "Dallas Cowboys",
+    teamsPlayed: ["Dallas Cowboys"],
+    statistics: {
+      receptions: 750,
+      receivingYards: 11904,
+      receivingTouchdowns: 65,
+    },
+    yearsActive: "1988-1999",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHY5jsbXXlgRLFsXKExb1nZFVze7XvN453Qw&s",
+  },
+  {
+    id: 30,
+    name: "Gale Sayers",
+    team: "Chicago Bears",
+    teamsPlayed: ["Chicago Bears"],
+    statistics: {
+      rushingYards: 4956,
+      rushingTouchdowns: 39,
+      receptions: 112,
+      receivingYards: 1307,
+      receivingTouchdowns: 9,
+    },
+    yearsActive: "1965-1971",
+    photoUrl:
+      "https://static.clubs.nfl.com/image/private/t_editorial_landscape_mobile/bears/j0gpqphohy03azyjksjy.jpg",
+  },
+  {
+    id: 31,
+    name: "John Elway",
+    team: "Denver Broncos",
+    teamsPlayed: ["Denver Broncos"],
+    statistics: {
+      completions: 4123,
+      attempts: 7250,
+      passingYards: 51475,
+      passingTouchdowns: 300,
+      interceptions: 226,
+    },
+    yearsActive: "1983-1998",
+    photoUrl:
+      "https://bloximages.newyork1.vip.townnews.com/denvergazette.com/content/tncms/assets/v3/editorial/1/8b/18b33118-a7ea-11ed-afff-9b51140a9a48/63e3fd73aa37a.image.jpg?resize=674%2C500",
+  },
+  {
+    id: 32,
+    name: "Marshall Faulk",
+    team: "St. Louis Rams",
+    teamsPlayed: ["Indianapolis Colts", "St. Louis Rams"],
+    statistics: {
+      rushingYards: 12279,
+      rushingTouchdowns: 100,
+      receptions: 767,
+      receivingYards: 6875,
+      receivingTouchdowns: 36,
+    },
+    yearsActive: "1994-2005",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGVZS8FfUuM_U3dOKUH8vv70OHwQE0vvb6Og&s",
+  },
+  {
+    id: 33,
+    name: "Warren Moon",
+    team: "Houston Oilers",
+    teamsPlayed: [
+      "Houston Oilers",
+      "Minnesota Vikings",
+      "Seattle Seahawks",
+      "Kansas City Chiefs",
+    ],
+    statistics: {
+      completions: 3988,
+      attempts: 6823,
+      passingYards: 49325,
+      passingTouchdowns: 291,
+      interceptions: 233,
+    },
+    yearsActive: "1984-2000",
+    photoUrl:
+      "https://e0.365dm.com/20/11/1600x900/skysports-warren-moon-houston-oilers_5176037.jpg?20201118115644",
+  },
+  {
+    id: 34,
+    name: "Alan Page",
+    team: "Minnesota Vikings",
+    teamsPlayed: ["Minnesota Vikings", "Chicago Bears"],
+    statistics: {
+      sacks: 148.5,
+      safeties: 3,
+      fumbleRecoveries: 23,
+    },
+    yearsActive: "1967-1981",
+    photoUrl:
+      "https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/vikings/trnvdctwlkl4yi9zpksw",
+  },
+  {
+    id: 35,
+    name: "Len Dawson",
+    team: "Kansas City Chiefs",
+    teamsPlayed: [
+      "Pittsburgh Steelers",
+      "Cleveland Browns",
+      "Kansas City Chiefs",
+    ],
+    statistics: {
+      completions: 2396,
+      attempts: 4180,
+      passingYards: 28711,
+      passingTouchdowns: 239,
+      interceptions: 183,
+    },
+    yearsActive: "1957-1975",
+    photoUrl:
+      "https://static.clubs.nfl.com/image/private/f_auto/chiefs/sz3pygnngxyvpw3psr6u",
+  },
+  {
+    id: 36,
+    name: "Jim Kelly",
+    team: "Buffalo Bills",
+    teamsPlayed: ["Buffalo Bills"],
+    statistics: {
+      completions: 2874,
+      attempts: 4779,
+      passingYards: 35467,
+      passingTouchdowns: 237,
+      interceptions: 175,
+    },
+    yearsActive: "1986-1996",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5k6D2wIIiepFI7RHEb5Aac16bCG9xI8JhBQ&s",
+  },
+  {
+    id: 37,
+    name: "Derrick Thomas",
+    team: "Kansas City Chiefs",
+    teamsPlayed: ["Kansas City Chiefs"],
+    statistics: {
+      sacks: 126.5,
+      forcedFumbles: 41,
+      fumbleRecoveries: 19,
+    },
+    yearsActive: "1989-1999",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_jz0Yfm70bo8-lit4iolgQtrzRfa3VZ0mdg&s",
+  },
+  {
+    id: 38,
+    name: "Paul Warfield",
+    team: "Miami Dolphins",
+    teamsPlayed: ["Cleveland Browns", "Miami Dolphins"],
+    statistics: {
+      receptions: 427,
+      receivingYards: 8565,
+      receivingTouchdowns: 85,
+    },
+    yearsActive: "1964-1977",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd_8hAOt0MCvxgKT8fTBZbsHIeEI291-0lFA&s",
+  },
+
+  {
+    id: 39,
+    name: "Marcus Allen",
+    team: "Los Angeles Raiders",
+    teamsPlayed: ["Los Angeles Raiders", "Kansas City Chiefs"],
+    statistics: {
+      rushingYards: 12243,
+      rushingTouchdowns: 123,
+      receptions: 587,
+      receivingYards: 5411,
+      receivingTouchdowns: 21,
+    },
+    yearsActive: "1982-1997",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_2q4mu-5bQg-M9fNvgaRp_rdhiajAWrzLjg&s",
+  },
+];
